a3c07013b8d3cfb3c82596698e2d7218156f20f2
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-31 02:16:20 +0530

    [Aniket]LogicalProblems

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Day6_LogicalProblems
+{
+    class Program
+    {        
+        public static void Main(string[] args)
+        {
+            FibonacciSeries series = new FibonacciSeries();
+            series.Fibonacci();
+
+
+            PerfectNumber perfectNumber = new PerfectNumber();
+            perfectNumber.PerfectNum();
+
+            PrimeNumber primeNumber = new PrimeNumber();
+            primeNumber.PrimeNo();
+
+            ReverseNumber reverseNumber = new ReverseNumber();
+            reverseNumber.ReverseNum();
+
+            CouponNumbers couponNumbers = new CouponNumbers();
+            couponNumbers.CalcCoupon();
+
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.Stopwatch();
+
+            DaysOfWeek daysOfWeek = new DaysOfWeek();
+            daysOfWeek.ReadInput();
+        }
+    }
+}
