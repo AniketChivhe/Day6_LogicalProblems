37dd07900a2d7f4854fde0dbbdd0a07e18418894
“AniketChivhe” <aniketchivhe@gmail.com>
2022-06-05 00:54:02 +0530

    [Aniket]Reverse number added

---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_LogicalProblems
+{
+    internal class ReverseNumber
+    {
+        public void ReverseNum()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter A Number To Check Reverse: ");
+            Console.WriteLine("-----------------------------------------------");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.Write("Reversed Number Is: " + reverse);
+        }
+    }
+}
