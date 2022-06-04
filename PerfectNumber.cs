c9f9f5e77cde13ea6b8d0ce4aede712445df6662
“AniketChivhe” <aniketchivhe@gmail.com>
2022-06-05 00:51:41 +0530

    [Aniket]Perfect number added

---
+++ PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_LogicalProblems
+{
+    internal class PerfectNumber
+    {
+        public void PerfectNum()
+        {
+            int  sum = 0, n;
+            ///enter a Number 
+            Console.Write("Enter the Number:\n");
+            Console.WriteLine("-----------------------------------------------");
+            int number = Convert.ToInt32(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+
+            }
+        }
+    }
+}
